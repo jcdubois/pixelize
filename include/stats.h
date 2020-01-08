1860e3aa4bbdc35b3f490bd61e000c3bbb484055
Jean-Christophe Dubois <jcd@tribudubois.net>
2020-01-08 02:13:04 +0100

    Fix header files.
    Signed-off-by: Jean-Christophe Dubois <jcd@tribudubois.net>

--- include/stats.h
+++ include/stats.h
@@ -25,6 +25,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include <gtk/gtk.h>
 
-gboolean calc_mead_std(double *data, int n, double *mean, double *std);
+gboolean calc_mead_std(const double *data, int n, double *mean, double *std);
 
 #endif
--- include/status.h
+++ include/status.h
@@ -32,6 +32,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 void set_progress_indicator(double val);
 void refresh_mode_display();
 
-void setup_status(GtkWidget *parent);
+void setup_status(GtkWidget *dialog);
 
 #endif
